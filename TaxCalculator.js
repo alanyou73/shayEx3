3bc2687bd1dd676436fd36f5bc49edc475f9beac
alany <alanyou73@gmail.com>
2019-01-06 18:28:27 +0200

    changes

---
+++ TaxCalculator.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    var ckbox = $('#checkbox');
+
+    $('input').on('click',function () {
+        if (ckbox.is(':checked')) {
+           $("td").hide();
+            $("#BS").show();
+            $("#BP").show();
+            $("#inputBS").show();
+            $("#inputBP").show();
+            $(".cb").show();
+        } else {
+            $("td").show();
+
+        }
+    });
+});
